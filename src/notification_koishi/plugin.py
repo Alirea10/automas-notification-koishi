a020246b0bbbf13a6c21242189c0df0b0baaaec2
Alirea <2981883527@qq.com>
2026-05-06 11:26:44 +0800

    feat(plugin): 移动plugin软件包

--- src/notification_koishi/plugin.py
+++ src/notification_koishi/plugin.py
@@ -5,7 +5,7 @@ from typing import Any, TYPE_CHECKING
 from app.utils.websocket import ws_client_manager
 
 if TYPE_CHECKING:
-    from mas.plugins import PluginContext
+    from app.plugins import PluginContext
 
 from .schema import Config
 
